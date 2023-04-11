5e808af5adc56e4e154181b9eb3b278da490592b
oppressor117 <oppressor117@gmail.com>
2023-04-11 20:55:00 +0300

    Corrections made

--- Lesson_10/Task_4.py
+++ Lesson_10/Task_4.py
@@ -1,7 +1,7 @@
 word_list = ['разработка', 'администрирование', 'protocol', 'standard']
 print(word_list)
-for line in word_list:
-     word_bytes = line.encode('utf-8')
-     print(f'В байтовом представлении: {word_bytes}')
+for word in word_list:
+     word_bytes = word.encode('utf-8')
+     print(f'В байтовом представлении: {word_bytes.hex()}')
      word_str = bytes.decode(word_bytes, 'utf-8')
      print(f'В строковом представлении: {word_str} \n')
